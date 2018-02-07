301a4bbf43efe1a837da675d7a4081ec7543f313
fumikistrider <fumikistrider@gmail.com>
2018-02-07 21:49:16 +0900

    1ファイルにまとめる

--- osxSketch/src/main.cpp
+++ osxSketch/src/main.cpp
@@ -1,6 +1,26 @@
 #include "ofMain.h"
-#include "ofApp.h"
 
+//#include "ofApp.h"
+
+
+class ofApp : public ofBaseApp{
+    
+public:
+    //--------------------------------------------------------------
+    void setup(){
+        ofBackground(0);
+    }
+    //--------------------------------------------------------------
+    void update(){
+    }
+    //--------------------------------------------------------------
+    void draw(){
+        ofDrawCircle(ofGetWidth()/2, ofGetHeight()/2, ofGetWidth()/4 * sin( ofDegToRad( ofGetFrameNum())));
+    }
+
+};
+
+ 
 //========================================================================
 int main( ){
 	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
